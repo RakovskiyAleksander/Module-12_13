44371ad877ea569185fd789382082126a43351ad
RakovskiyAleksander <lopa20000@gmail.com>
2025-11-10 21:34:38 +0300

    Доработал Camera Follow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,25 +3,21 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     private GameObject _target;
-    private float _cameraSpeedRotation;
+    private float _cameraSpeedFollowRotation;
     private float _cameraSpeedFollow;
     private Vector3 _directionMovement;
 
-    public void Initialize(GameObject target,float cameraSpeedRotation, float cameraSpeedFollow)
+    public void Initialize(GameObject target, float cameraSpeedRotation, float cameraSpeedFollow)
     {
         _target = target;
-        _cameraSpeedRotation = cameraSpeedRotation; 
+        _cameraSpeedFollowRotation = cameraSpeedRotation;
         _cameraSpeedFollow = cameraSpeedFollow;
-        _directionMovement = _target.GetComponent<Player>().DirectionMovement;
     }
 
     void LateUpdate()
     {
-        // transform.rotation = Quaternion.Lerp(Quaternion.Euler( _directionMovement), _target.transform.rotation, _cameraSpeedRotation * Time.deltaTime);
-        //transform.rotation = Quaternion.Euler(_target.GetComponent<Player>().DirectionMovement);
-        Debug.Log(_target.GetComponent<Player>().DirectionMovement);
-        //transform.LookAt(_target.GetComponent<Player>().DirectionMovement);
-        transform.rotation = Quaternion.LookRotation(_target.GetComponent<Player>().DirectionMovement);
+        _directionMovement = _target.GetComponent<Player>().DirectionMovement;
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(_directionMovement), _cameraSpeedFollowRotation * Time.fixedDeltaTime);
         transform.position = Vector3.Lerp(transform.position, _target.transform.position, _cameraSpeedFollow * Time.deltaTime);
     }
 }
